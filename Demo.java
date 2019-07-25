02592c53ce97865c1b8bc4ebe32c4417ba2b8381
qiannan.qu <qiannan.qu@accenture.com>
2019-07-25 14:31:41 +0800

    demo

--- Demo.java
+++ Demo.java
@@ -1,20 +1,14 @@
 package demo;
 
-import java.util.Iterator;
 
 public class Demo {
 	
 	
 	
 	public static void main(String[] args) {
-		String[] array = {"001", "002", "003"};
-		Sequence seq = new Sequence(array);
-		seq.append("004");
-		seq.insert(0, "005");
-		Iterator<String> iterator = seq.iterator();
-		while(iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
+		String string = "1234,1234,1234";
+		boolean matches = string.matches("([0-9]{4}[,]?)+");
+		System.out.println(matches);
 	}
 
 }
